a445971fae9d976608a0ca98619ac97545ba8997
Debaleena Roy <debaleena.roy2@cognizant.com>
2021-10-29 00:09:53 +0530

    First Commit

---
+++ src/FirstPackage/Test1.java
@@ -0,0 +1,15 @@
+package FirstPackage;
+
+public class Test1 {
+	public static void main (String args[]) {
+		String dest = "SIP";
+		if (dest == "SIP")
+			System.out.println("************Please provide URL************");	
+	    else {
+		    System.out.println("#########Something Wrong#############");
+	}
+		
+	System.out.println();
+
+}
+}
